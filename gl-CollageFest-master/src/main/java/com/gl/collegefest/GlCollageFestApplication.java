f24193f2431f6f84810f136a13a39cea64960223
devanathan1234 <108325910+devanathan1234@users.noreply.github.com>
2023-02-05 22:27:17 +0530

    Add files via upload

---
+++ gl-CollageFest-master/src/main/java/com/gl/collegefest/GlCollageFestApplication.java
@@ -0,0 +1,14 @@
+package com.gl.collegefest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlCollageFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlCollageFestApplication.class, args);
+		System.out.println("Welcome");
+	}
+
+}
---
+++ gl-CollageFest-master/src/test/java/com/gl/collagefest/GlCollageFestMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.collagefest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlCollageFestMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
